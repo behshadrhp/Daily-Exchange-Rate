4ae02172377770afe59cc775d90fbca56e5f1670
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-02-14 06:51:04 +0330

    convert: Class Based

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import requests
 from conf import URL
 
 
-def get_rate(url):
-    response = requests.get(url)
-    data = response.json()
-    print(data)
+class DailyExchangeRate:
+
+    def get_rate(url):
+        response = requests.get(url)
+        data = response.json()
+        print(data)
 
 
 if __name__ == '__main__':
-    get_rate(URL)
+    DailyExchangeRate.get_rate(URL)
