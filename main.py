0cec082b5dd85b3e010e42b950fb675ebe82eb5b
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2023-02-14 07:05:36 +0330

    check&management: check response and management error

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ from conf import URL
 class DailyExchangeRate:
 
     def get_rate(url):
-        response = requests.get(url)
-        data = response.json()
-        print(data)
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                return data
+            else:
+                return 'The operation was not successful. Please try again ...'
+        except:
+            return 'We encountered some problems while performing the operation. Please check your internet connection and try again'
 
 
 if __name__ == '__main__':
-    DailyExchangeRate.get_rate(URL)
+    print(DailyExchangeRate.get_rate(URL))
